5e382fe738622bab499678b8c5c6279068a89285
Aslan Bennington-Barr <aslan.bennington.barr@gmail.com>
2024-01-11 21:22:04 +1300

    Sveltekit template setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<div class="root">
+	<div class="child">
+		<h1>Simplify Life</h1>
+		<h2>Maximise Living</h2>
+	</div>
+</div>
+
+<style>
+	:global(body) {
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		background-color: #22092c;
+		height: 100%;
+	}
+
+	:global(body div) {
+		height: 100%;
+		background-color: red;
+	}
+	div.root {
+		color: white;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	div.child {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
