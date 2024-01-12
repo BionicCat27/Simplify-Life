69cca399859c5afa0bc79186a1c77590c2ad18d7
abenningtonbarr <aslan.bennington.barr@gmail.com>
2024-01-12 21:43:06 +1300

    Capacitor for Android static config

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'earth.aslanbb.simplifylife',
+  appName: 'simplify-life',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
